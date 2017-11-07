816425f6847150427141b92efffdd4a1640da361
tukangzheng <tukangzheng@gomeplus.com>
2017-11-07 20:30:06 +0800

    添加注释

--- src/main/java/com/nettyrpc/protocol/RpcDecoder.java
+++ src/main/java/com/nettyrpc/protocol/RpcDecoder.java
@@ -22,18 +22,24 @@ public class RpcDecoder extends ByteToMessageDecoder {
         if (in.readableBytes() < 4) {
             return;
         }
+        // 标记ByteBuf的readerIndex()的位置
         in.markReaderIndex();
         int dataLength = in.readInt();
-        /*if (dataLength <= 0) {
-            ctx.close();
-        }*/
+
+        /**
+         * readableBytes 返回可被读取的字节数
+         * writeableBytes 返回可被写入的字节数
+         */
         if (in.readableBytes() < dataLength) {
+            // 重置ByteBuf的readerIndex()的位置
             in.resetReaderIndex();
             return;
         }
         byte[] data = new byte[dataLength];
+        // 将ByteBuf中的数据读到data字节数组当做
         in.readBytes(data);
 
+        // 对该字节数组进行反序列化
         Object obj = SerializationUtil.deserialize(data, genericClass);
         //Object obj = JsonUtil.deserialize(data,genericClass); // Not use this, have some bugs
         out.add(obj);
